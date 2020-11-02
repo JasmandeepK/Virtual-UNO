ab5ccec986f764e9a75df11eff92354009a4196c
gehowes <g_howes1@u.pacific.edu>
2020-11-02 10:21:54 -0800

    completed the colorType enum file

--- group-project/src/starter/ColorType.java
+++ group-project/src/starter/ColorType.java
@@ -1,5 +1,16 @@
 package starter;
 
 public enum ColorType {
-
+	RED, GREEN, BLUE, YELLOW, WILD;
+	
+	public String toString() {
+		switch(this) {
+			case RED: return "red";
+			case GREEN: return "green";
+			case BLUE: return "blue";
+			case YELLOW: return "yellow";
+			case WILD: return "wild";
+		}
+		return "n/a";
+	}
 }
