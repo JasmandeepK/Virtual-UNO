607f9057121a1a38426b0054a4373c982a573326
Jasmandeep Kaur <kaurj1304@gmail.com>
2020-11-02 10:28:22 -0800

    Added comments about what the Colortype enum class is about

--- group-project/src/starter/ColorType.java
+++ group-project/src/starter/ColorType.java
@@ -1,3 +1,11 @@
+/**
+ * This file is an enum class for the color type for the card type.
+ * There are 5 color types: red, green, blue, yellow, and wild.
+ * 
+ * @author Jasmandeep Kaur, Thyphien Son, Gwendolyn Howes, Samantha Sousa
+ *
+ */
+
 package starter;
 
 public enum ColorType {
