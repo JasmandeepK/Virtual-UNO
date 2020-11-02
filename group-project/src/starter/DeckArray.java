34e55400e84c17f6b2844604374c0592d5d4ce82
Thyphien Son <t_son4@u.pacific.edu>
2020-11-01 21:25:04 -0800

    Added code to DeckArray.Java

---
+++ group-project/src/starter/DeckArray.java
@@ -0,0 +1,25 @@
+package starter;
+import java.util.ArrayList;
+import javax.smartcardio.Card;
+
+
+
+
+class Deck {
+
+	ArrayList<Card> pile;
+	ArrayList<Card> cards;
+
+	public Deck(ArrayList<Card> c) {
+		cards = c;
+	}
+
+	public Deck() {
+		cards = new ArrayList<Card>(108);
+	}
+
+	public void addCard(Card c) {
+		cards.add(c);
+	}
+
+}
