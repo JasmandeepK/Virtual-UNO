928e55b4e511ce5d9d2fe783e73bf33c792cb3a3
Jasmandeep Kaur <kaurj1304@gmail.com>
2020-11-04 21:12:26 -0800

    Changed ColorType from an enum to a class
    This will allow for easier usage of the data. Switch statements are
    changed to if statements and a color variable is added.

--- group-project/src/starter/ColorType.java
+++ group-project/src/starter/ColorType.java
@@ -1,5 +1,5 @@
 /**
- * This file is an enum class for the color type for the card type.
+ * This file is a class for the color type for the card type.
  * There are 5 color types: red, green, blue, yellow, and wild.
  * 
  * @author Jasmandeep Kaur, Thyphien Son, Gwendolyn Howes, Samantha Sousa
@@ -8,16 +8,28 @@
 
 package starter;
 
-public enum ColorType {
-	RED, GREEN, BLUE, YELLOW, WILD;
+public class ColorType {
+	private int color;
 	
-	public String toString() {
-		switch(this) {
-			case RED: return "red";
-			case GREEN: return "green";
-			case BLUE: return "blue";
-			case YELLOW: return "yellow";
-			case WILD: return "wild";
+	public void setColor(int num) {
+		this.color = num;
+	}
+	
+	public String getColor() {
+		if(color == 0) {
+			return "red";
+		}
+		else if(color == 1) {
+			return "yellow";
+		}
+		else if(color == 2) {
+			return "blue";
+		}
+		else if(color == 3) {
+			return "green";
+		}
+		else if(color == 4){
+			return "wild";
 		}
 		return "n/a";
 	}
