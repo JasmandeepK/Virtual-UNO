d3a5bb5c29f08be0ce02c5aa247f665615ac8ce0
Jasmandeep Kaur <kaurj1304@gmail.com>
2020-11-04 21:13:53 -0800

    Added functions and variables to CardValue
    Similarly to ColorType, CardValue is now a class and no longer an enum.
    It holds the data regarding the valuse of the card.

--- group-project/src/starter/CardValue.java
+++ group-project/src/starter/CardValue.java
@@ -1,5 +1,31 @@
 package starter;
 
-public enum CardValue {
-
+public class CardValue {
+	private int value;
+	
+	public void setValue(int num) {
+		this.value = num;
+	}
+	
+	public String getValue() {
+		if(value >= 0 && value <= 9) {
+			return Integer.toString(value);
+		}
+		else if(value == 10) {
+			return "+2";
+		}
+		else if(value == 11) {
+			return "reverse";
+		}
+		else if(value == 12) {
+			return "skip";
+		}
+		else if(value == 13){
+			return "wild";
+		}
+		else if (value == 14) {
+			return "+4";
+		}
+		return "n/a";
+	}
 }
