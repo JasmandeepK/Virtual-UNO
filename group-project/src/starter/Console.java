51697e38ffc9b48b21a16aa43aa7c889905d272c
Samantha Sousa <sammy.sousa@yahoo.com>
2020-11-13 09:51:52 -0800

    Made small changes to console, gameplay, deckarray, and handarray.

---
+++ group-project/src/starter/Console.java
@@ -0,0 +1,10 @@
+package starter;
+
+//console -- main:void -- playGame() -- getHand()
+public class Console {
+	
+	public static void main(String[] args) {
+
+	}
+	
+}
---
+++ group-project/src/starter/GamePlay.java
@@ -0,0 +1,16 @@
+package starter;
+
+//actionCard -- specialCard -- winCondition -- validation -- player1Turn -- player2Turn
+
+public class GamePlay {
+	
+	//Constructor
+	public GamePlay() {
+		
+	}
+	
+	//Verify that the current player has no remaining cards
+	public boolean emptyHand() {
+		return false; //Will change and add more later
+	}
+}
