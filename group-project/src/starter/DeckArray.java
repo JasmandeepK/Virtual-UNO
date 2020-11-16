1af7649c4106b0d3527754375695a687e03156a2
gehowes <g_howes1@u.pacific.edu>
2020-11-16 10:58:49 -0800

    made a queue class for our cards to be enqueued and dequeued

--- group-project/src/starter/DeckArray.java
+++ group-project/src/starter/DeckArray.java
@@ -2,7 +2,79 @@ package starter;
 import java.util.ArrayList;
 import javax.smartcardio.Card;
 
-class Deck {
+class Queue {
+	int front, rear, size;
+	int capacity;
+	int array[];
+	
+	public static void main(String[] args) {
+		Queue queue = new Queue(5);
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+		queue.enqueue(4);
+		
+		System.out.println (queue.dequeue() + " dequeued form queque");
+		System.out.println (queue.front() + " from the front of queue");
+		System.out.println (queue.rear() + " from the back of queue");
+	}
+	
+	public Queue(int capacity) {
+		this.capacity = capacity;
+		front = this.size = 0;
+		rear = capacity - 1;
+		array = new int[this.capacity];
+	}
+	
+	boolean isFull (Queue queue) {
+		return (queue.size == queue.capacity);
+	}
+	
+	boolean isEmpty (Queue queue) {
+		return (queue.size == 0);
+	}
+	
+	void enqueue (int item)  {
+		if (isFull(this))
+			return;
+		this.rear = (this.rear + 1) % this.capacity;
+		this.array[this.rear] = item;
+		this.size = this.size + 1;
+	}
+	
+	int dequeue() {
+		if (isEmpty(this)) 
+			return Integer.MIN_VALUE;
+		int item = this.array[this.front];
+		this.front = (this.front + 1) % this.capacity;
+		this.size = this.size - 1;
+		return item;
+	}
+	
+	int front () {
+		if (isEmpty(this))
+			return Integer.MIN_VALUE;
+		
+		return this.array[this.front];
+	}
+	
+	int rear () {
+		if (isEmpty(this))
+			return Integer.MIN_VALUE;
+		
+		return this.array[this.rear];
+	}
+	
+ }
+
+
+
+
+
+
+
+
+/*Deck {
 
 	ArrayList<Card> pile;
 	ArrayList<Card> cards;
@@ -19,4 +91,4 @@ class Deck {
 		cards.add(c);
 	}
 
-}
+}*/
