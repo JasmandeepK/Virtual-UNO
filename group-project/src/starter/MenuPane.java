ca99e505c556b08ac0d052cb3686b9ec42509258
Jasmandeep Kaur <kaurj1304@gmail.com>
2020-11-30 18:44:47 -0800

    Play button/label implemented
    Added in a glabel for the play text and changed the color of the
    gbutton. Also renamed the rect to playButton for easier future
    reference.

--- group-project/src/starter/MenuPane.java
+++ group-project/src/starter/MenuPane.java
@@ -2,35 +2,46 @@ package starter;
 
 import java.awt.Color;
 import java.awt.event.MouseEvent;
-
+import java.awt.*;
+import acm.graphics.GLabel;
 import acm.graphics.GObject;
 
 public class MenuPane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
-	private GButton rect;
-
+	private GButton playButton;
+	private GLabel playLabel;
+	
 	public MenuPane(MainApplication app) {
 		super();
 		program = app;
-		rect = new GButton("Next", 200, 200, 200, 200);
-		rect.setFillColor(Color.RED);
+		playButton = new GButton("", 200, 200, 100, 50);
+		playButton.setFillColor(Color.YELLOW);
+		playLabel = new GLabel("PLAY", 220, 235);
+		playLabel.setFont("TimesRoman-Bold-24");
+		playLabel.setColor(Color.WHITE);
+		
 	}
 
 	@Override
 	public void showContents() {
-		program.add(rect);
+		program.add(playButton);
+		program.add(playLabel);
 	}
 
 	@Override
 	public void hideContents() {
-		program.remove(rect);
+		program.remove(playButton);
+		program.remove(playLabel);
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if (obj == rect) {
+		if (obj == playButton) {
+			program.switchToSome();
+		}
+		if (obj == playLabel) {
 			program.switchToSome();
 		}
 	}
