89228613bd05547ac0ad053866268b6244dd085d
Jasmandeep Kaur <kaurj1304@gmail.com>
2020-12-03 18:02:59 -0800

    added name GLabel
    still needs fixing for the name label - location, size, user input

--- group-project/src/starter/UsernamePane.java
+++ group-project/src/starter/UsernamePane.java
@@ -13,6 +13,7 @@ public class UsernamePane extends GraphicsPane {
 	private GButton nextButton;
 	private GLabel nextLabel;
 	private GLabel prompt;
+	private GLabel name;
 	
 	public UsernamePane(MainApplication app) {
 		super();
@@ -27,19 +28,29 @@ public class UsernamePane extends GraphicsPane {
 		nextLabel.setFont("TimesRoman-Bold-24");
 		nextLabel.setColor(Color.WHITE);
 		
+		//prompt
+		prompt = new GLabel("ENTER IN A USERNAME FOR PLAYER ONE:", 100,100);
+		prompt.setFont("TimesRoman-Bold-30");
+		
+		//user input for username
+		name = new GLabel("", 200, 200);
+		name.setColor(Color.YELLOW);
 	}
 
 	@Override
 	public void showContents() {
 		program.add(nextButton);
 		program.add(nextLabel);
+		program.add(prompt);
+		program.add(name);
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(nextButton);
 		program.remove(nextLabel);
-		//program.remove(prompt);
+		program.remove(prompt);
+		program.remove(name);
 	}
 
 	@Override
