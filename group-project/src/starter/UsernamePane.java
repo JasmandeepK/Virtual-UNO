0824e6ee210da50b7c7e1a917bf04c2a23446db2
Thyphien Son <t_son4@u.pacific.edu>
2020-12-04 20:39:33 -0800

    resized for a better fit

--- group-project/src/starter/UsernamePane.java
+++ group-project/src/starter/UsernamePane.java
@@ -1,6 +1,7 @@
 package starter;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.Color;
@@ -11,14 +12,14 @@ import java.awt.*;
 
 import acm.graphics.*;
 
-public class UsernamePane extends GraphicsPane {
+public class UsernamePane extends GraphicsPane implements ActionListener {
 	private MainApplication program;
 	
 	private GButton nextButton;
 	private GLabel nextLabel;
 	private GLabel prompt;
 	private JTextField userInputted;
-	private static final Font initFont= new Font("Arial", Font.BOLD, 25);
+	private static final Font initFont= new Font("Arial", Font.BOLD, 50);
 	private String input = "";
 	
 	public UsernamePane(MainApplication app) {
@@ -28,20 +29,20 @@ public class UsernamePane extends GraphicsPane {
 		userInputted = new JTextField("Enter username:");
 		userInputted.setFont(initFont);
 		userInputted.setEnabled(true);
-		userInputted.setBounds(200, 200, 500, 50);
+		userInputted.setBounds(400, 300, 1000, 100);
 		
 		//next label
-		nextButton = new GButton("", 325, 300, 100, 50);
+		nextButton = new GButton("", 825, 600, 150, 70);
 		nextButton.setFillColor(Color.YELLOW);
 		
 		//next label on top of next button
-		nextLabel = new GLabel("NEXT", 345, 335);
-		nextLabel.setFont("TimesRoman-Bold-24");
+		nextLabel = new GLabel("NEXT", 837, 650);
+		nextLabel.setFont("TimesRoman-Bold-50");
 		nextLabel.setColor(Color.WHITE);
 		
 		//prompt
-		prompt = new GLabel("ENTER IN A USERNAME FOR PLAYER ONE:", 100,100);
-		prompt.setFont("TimesRoman-Bold-30");
+		prompt = new GLabel("ENTER IN A USERNAME FOR PLAYER ONE:", 150,150);
+		prompt.setFont("TimesRoman-Bold-70");
 		
 		
 	}
