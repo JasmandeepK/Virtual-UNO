5626638da84a7a510ec0b96cd92ecbbc6a1f73e5
Jasmandeep Kaur <kaurj1304@gmail.com>
2020-12-05 15:22:38 -0800

    added playagain button

--- group-project/src/starter/WinPane.java
+++ group-project/src/starter/WinPane.java
@@ -1,24 +1,42 @@
 package starter;
 
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GImage;
+import acm.graphics.GLabel;
+import acm.graphics.GObject;
+
 public class WinPane extends GraphicsPane {
 	private MainApplication program;
-	
+	private GLabel winLabel;
+	private GButton playAgain;
 	
 	public WinPane(MainApplication app) {
 		super();
 		program = app;
+		
+		playAgain = new GButton("Play Again", 200, 200, 200, 200);
+		
 	}
 	
 	@Override
 	public void showContents() {
 		// TODO Auto-generated method stub
+		program.add(playAgain);
 		
 	}
 
 	@Override
 	public void hideContents() {
 		// TODO Auto-generated method stub
-		
+		program.remove(playAgain);
 	}
-
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		GObject obj = program.getElementAt(e.getX(), e.getY());
+		if (obj == playAgain) {
+			hideContents();
+		}
+	}
 }
