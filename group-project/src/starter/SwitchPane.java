99a8886acbaf8e45ec1d87c2e74ecd255a162ffe
Jasmandeep Kaur <kaurj1304@gmail.com>
2020-12-05 23:11:01 -0800

    skeleton code

--- group-project/src/starter/SwitchPane.java
+++ group-project/src/starter/SwitchPane.java
@@ -1,5 +1,17 @@
 package starter;
 
-public class SwitchPane {
+public class SwitchPane extends GraphicsPane{
+
+	@Override
+	public void showContents() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hideContents() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
