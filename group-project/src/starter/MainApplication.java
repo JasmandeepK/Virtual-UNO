995bfe207d6d30ae9ca39a96195fcb3c20a92da7
Jasmandeep Kaur <kaurj1304@gmail.com>
2020-12-05 21:48:32 -0800

    got rid of sound in mainapplication

--- group-project/src/starter/MainApplication.java
+++ group-project/src/starter/MainApplication.java
@@ -2,24 +2,18 @@ package starter;
 public class MainApplication extends GraphicsApplication {
 	public static final int WINDOW_WIDTH = 1800;
 	public static final int WINDOW_HEIGHT = 900;
-	public static final String MUSIC_FOLDER = "sounds";
-	private static final String[] SOUND_FILES = { "r2d2.mp3", "somethinlikethis.mp3" };
 
-	private SomePane somePane;
 	private MenuPane menu;
 	private UsernamePane usernameScreen;
 	private UsernamePane2 usernameScreen2;
 	private UnoPane unoPane;
 	private WinPane winPane;
-	private int count;
 
 	public void init() {
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 	}
 
 	public void run() {
-		System.out.println("Hello, world!");
-		somePane = new SomePane(this);
 		menu = new MenuPane(this);
 		usernameScreen = new UsernamePane(this);
 		usernameScreen2 = new UsernamePane2(this);
@@ -49,8 +43,6 @@ public class MainApplication extends GraphicsApplication {
 	}
 	
 	public void switchToMenu() {
-		//playRandomSound();
-		count++;
 		switchToScreen(menu);
 	}
 	
@@ -68,15 +60,5 @@ public class MainApplication extends GraphicsApplication {
 	
 	public void switchToWinPane(){
 		switchToScreen(winPane);
-	}
-	
-	public void switchToSome() {
-		//playRandomSound();
-		switchToScreen(somePane);
-	}
-
-	private void playRandomSound() {
-		AudioPlayer audio = AudioPlayer.getInstance();
-		audio.playSound(MUSIC_FOLDER, SOUND_FILES[count % SOUND_FILES.length]);
 	}
 }
