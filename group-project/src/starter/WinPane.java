90b358fbc42e09f29757790c22172d33f4903478
Jasmandeep Kaur <kaurj1304@gmail.com>
2020-12-05 23:42:45 -0800

    added balloons

--- group-project/src/starter/WinPane.java
+++ group-project/src/starter/WinPane.java
@@ -2,17 +2,13 @@ package starter;
 
 import java.awt.event.MouseEvent;
 
-import acm.graphics.GImage;
-import acm.graphics.GLabel;
-import acm.graphics.GObject;
+import acm.graphics.*;
 
 public class WinPane extends GraphicsPane {
 	private MainApplication program;
 	private GLabel winLabel;
 	private GLabel thanks;
-	private GImage balloon1;
-	private GImage balloon2;
-	private GImage balloon3;
+	private GImage balloon;
 	
 	public WinPane(MainApplication app) {
 		super();
@@ -22,14 +18,14 @@ public class WinPane extends GraphicsPane {
 		winLabel.setFont("TimesRoman-Bold-50");
 		thanks = new GLabel(" Thanks for Playing UNO", 650, 500);
 		thanks.setFont("TimesRoman-Bold-50");
-		balloon1 = new GImage("balloons.webp", 200, 200);
-		balloon1.setSize(500, 500);
+		balloon = new GImage("balloons.png", 200, 0);
+		balloon.setSize(1400, 900);
 	}
 	
 	@Override
 	public void showContents() {
 		// TODO Auto-generated method stub
-		program.add(balloon1);
+		program.add(balloon);
 		program.add(winLabel);
 		program.add(thanks);
 		
@@ -38,7 +34,7 @@ public class WinPane extends GraphicsPane {
 	@Override
 	public void hideContents() {
 		// TODO Auto-generated method stub
-		program.remove(balloon1);
+		program.remove(balloon);
 		program.remove(winLabel);
 		program.remove(thanks);
 	}
