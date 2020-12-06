62312e9ef177d3cdfadebd0f6de85cc9b5a091bf
Jasmandeep Kaur <kaurj1304@gmail.com>
2020-12-05 23:18:43 -0800

    added winLabel and thanks

--- group-project/src/starter/WinPane.java
+++ group-project/src/starter/WinPane.java
@@ -9,34 +9,32 @@ import acm.graphics.GObject;
 public class WinPane extends GraphicsPane {
 	private MainApplication program;
 	private GLabel winLabel;
-	private GButton playAgain;
+	private GLabel thanks;
 	
 	public WinPane(MainApplication app) {
 		super();
 		program = app;
 		
-		playAgain = new GButton("Play Again", 200, 200, 200, 200);
+		winLabel = new GLabel(program.getWinningPlayerName() + " Wins! Congratulations!!!!", 600, 200);
+		winLabel.setFont("TimesRoman-Bold-50");
+		thanks = new GLabel(" Thanks for Playing UNO", 650, 500);
+		thanks.setFont("TimesRoman-Bold-50");
+		
 		
 	}
 	
 	@Override
 	public void showContents() {
 		// TODO Auto-generated method stub
-		program.add(playAgain);
+		program.add(winLabel);
+		program.add(thanks);
 		
 	}
 
 	@Override
 	public void hideContents() {
 		// TODO Auto-generated method stub
-		program.remove(playAgain);
-	}
-	
-	@Override
-	public void mousePressed(MouseEvent e) {
-		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if (obj == playAgain) {
-			program.switchToMenu();
-		}
+		program.remove(winLabel);
+		program.remove(thanks);
 	}
 }
