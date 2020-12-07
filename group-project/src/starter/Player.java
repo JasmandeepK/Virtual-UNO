a9aa385d1f7d3da39c4ca08434bdce8d0ec41b68
Jasmandeep Kaur <kaurj1304@gmail.com>
2020-12-07 15:39:06 -0800

    added comments

--- group-project/src/starter/Player.java
+++ group-project/src/starter/Player.java
@@ -1,3 +1,4 @@
+//This class contains player data such as playername, hand, number, and the number of cards
 package starter;
 import java.util.*;
 
@@ -14,29 +15,37 @@ public class Player {
 		numberOfCards = 0;
 	}
 	
+	//sets the location of the card on the screen
 	public void setCoordinates(int i, int[] location) {
 		playerHand[i].setCoordinates(location);
 	}
 	
+	//returns the location of a card
 	public int[] getCoordinates(int i) {
 		return playerHand[i].getCoordinates();
 	}
 	
+	//sets the player name
 	public void setName(String name) {
 		playerName = name;
 	}
+	
+	//returns an array of cards which is the player's hand
 	public UnoCard[] getPlayerHand() {
 		return playerHand;
 	}
 	
+	//sets the player hand
 	public void setPlayerHand(UnoCard[] array) {
 		playerHand = array;
 	}
+	
 	//Get Player name from User
 	public String getPlayerName(){
 		return playerName;
 	}
 	
+	//gets the number of cards the player has
 	public int getNumCards() {
 		return numberOfCards;
 	}
@@ -47,6 +56,7 @@ public class Player {
 		playerHand[numberOfCards-1] = c;
 	}
 	
+	//removes a card from the player's hand
 	public void removeFromHand(int index){
 		for(int i = index;i < numberOfCards;i++){
 			if(i == numberOfCards-1) {
